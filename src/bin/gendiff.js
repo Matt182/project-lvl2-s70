e7f4a9d6639efb169c270b9fb9f9395b86b34ba0
lebedev <egor.lebedev@yumalabs.ru>
2017-03-21 00:23:48 +0300

    step 2

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,10 +1,15 @@
 #!/usr/bin/env node
 
 import commander from 'commander';
+import gendiff from '../';
 
 commander
-  .version('1.0.4')
+  .version('1.0.5')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'output format')
   .arguments('<first_config> <second_config>')
+  .action((firstConfig, secondConfig) => {
+    const output = gendiff(firstConfig, secondConfig);
+    console.log(output);
+  })
   .parse(process.argv);
