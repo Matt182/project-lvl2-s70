64f3cf740d06bbd2b1812304a28d581922403288
lebedev <egor.lebedev@yumalabs.ru>
2017-03-21 19:12:24 +0300

    step 3

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -4,7 +4,7 @@ import commander from 'commander';
 import gendiff from '../';
 
 commander
-  .version('1.0.5')
+  .version('1.1.0')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'output format')
   .arguments('<first_config> <second_config>')
